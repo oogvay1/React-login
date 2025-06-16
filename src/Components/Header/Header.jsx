df920c78bfdcb972470bc9ea1a42a7b3c03b77a4
oogvay1 <atalgat592@gmail.com>
2025-06-16 17:39:17 +0500

    Add header style

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -16,16 +16,23 @@ function Header() {
             <header className='header'>
                 <div className="container">
                     <div className="header-inner">
-                        <div className="headear-logo">
-                            <h1>INWOOD</h1>
-                        </div>
+                        <div className="header-main-part">
+                            <div className="headear-logo">
+                                <h1>INWOOD</h1>
+                            </div>
 
-                        <div className="header-contact">
-                            <button>Contact us</button>
+                            <div className="header-contact">
+                                <button>Contact us</button>
+                            </div>
                         </div>
 
                         <div onClick={() => openModal} className="header-menu">
-                            
+                            <div className="menu-spans">
+                                <div className="menu1 menu"></div>
+                                <div className="menu2 menu"></div>
+                                <div className="menu3 menu"></div>
+                            </div>
+                            <p>menu • menu • menu • menu menu • menu</p>
                         </div>
                     </div>
                 </div>
